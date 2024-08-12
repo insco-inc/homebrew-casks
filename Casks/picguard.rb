dd11200b35552bb3a2f70d50b7a94243c9ec3310
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2024-08-12 16:37:23 +0000

    picguard 1.0.0+378

--- Casks/picguard.rb
+++ Casks/picguard.rb
@@ -1,6 +1,6 @@
 cask "picguard" do
-  version "1.0.0+376"
-  sha256 "5f244c11aa11b127649ce2d1bb3f77412bfc36a520cf04aad0b175614766f0ce"
+  version "1.0.0+378"
+  sha256 "9c71c1b9bb229a00ffb945eff7081039f1bf8ca36e2b31e1bcabcacd8d91abdc"
 
   url "https://github.com/picguard/picguard/releases/download/v#{version}/PicGuard_#{version}_macos_universal.dmg",
       verified: "github.com/picguard/picguard/"
