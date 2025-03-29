487a713de3bc00b80882a189da6cf8bf17279ccf
kjxbyz <kjxbyz888@outlook.com>
2025-03-29 23:18:26 +0800

    fix: update asset name

--- Casks/picguard.rb
+++ Casks/picguard.rb
@@ -1,8 +1,8 @@
 cask "picguard" do
-  version "1.1.0+440"
-  sha256 "1918e9c14620f7b7da338e8fb16e7177cd5343aea91501256207cf53e2514ea9"
+  version "2.0.0+441"
+  sha256 "6cf7c38e6ba956d4ddd01594efb925c11d2a9a9676e66840946b875899adc1df"
 
-  url "https://github.com/picguard/picguard/releases/download/v#{version}/PicGuard_#{version}_macos_universal.dmg",
+  url "https://github.com/picguard/picguard/releases/download/v#{version}/PicGuard-#{version.tr("+", ".")}-macos-universal.dmg",
       verified: "github.com/picguard/picguard/"
   name "PicGuard"
   desc "Your pictures, your signature"
