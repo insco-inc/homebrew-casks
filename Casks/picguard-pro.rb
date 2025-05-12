e19798bc22bb1355792568c3411404cb88c2d86d
kjxbyz <kjxbyz888@outlook.com>
2025-05-12 20:58:10 +0800

    feat: add picguard-pro

---
+++ Casks/picguard-pro.rb
@@ -0,0 +1,34 @@
+cask "picguard-pro" do
+  version "2.5.4+447"
+  sha256 "06eb3473553d4c552bce5064e8a20a76bee4ae07c955825ab470e458f40cff98"
+
+  url "https://github.com/picguard/picguard/releases/download/v#{version}/PicGuard-Pro-#{version.tr("+", ".")}-macos-universal.dmg",
+      verified: "github.com/picguard/picguard/"
+  name "PicGuard Pro"
+  desc "Your pictures, your signature"
+  homepage "https://picguard.app/"
+
+  livecheck do
+    url :url
+    strategy :git do |tags|
+      tags.filter_map do |tag|
+        match = tag&.match(/^v?(\d+(?:\.\d+)+(\+\d+)?)$/i)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "PicGuard Pro.app"
+
+  zap trash: [
+    "~/Library/Application Support/PicGuard Pro",
+    "~/Library/Caches/com.kjxbyz.picguard.pro",
+    "~/Library/Caches/PicGuard Pro",
+    "~/Library/Preferences/com.kjxbyz.picguard.pro.plist",
+    "~/Library/Saved Application State/com.kjxbyz.picguard.pro.savedState",
+  ]
+end
